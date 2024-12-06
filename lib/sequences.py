d6ef850d0989afbac87d1fc2aeb8b876972c2a81
florence <loracodes3@gmail.com>
2024-12-06 10:35:18 +0300

    sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
-#!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+        return
+    elif length == 1:
+        print([0])
+        return
+    elif length == 2:
+        print([0, 1])
+        return
+    
+    # Generate the Fibonacci sequence for length > 2
+    fib_sequence = [0, 1]
+    while len(fib_sequence) < length:
+        fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+    
+    print(fib_sequence)
